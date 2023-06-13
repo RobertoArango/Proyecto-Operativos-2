cc2f828d5ac33f7b430a156da2ae26a45968672f
RobertoArango <roberto.arango@correo.unimet.edu.ve>
2023-06-13 18:05:29 -0400

    clase vehiculos listo (falta definir el parametro de los puntos)

--- src/proyecto_operativos_2/Vehiculos.java
+++ src/proyecto_operativos_2/Vehiculos.java
@@ -11,4 +11,56 @@ package proyecto_operativos_2;
  */
 public class Vehiculos {
     
+    public int id = 0;
+    public int nivel = 0;
+    public int contador = 0;
+    public int calidad = 0;
+    public int marca = 0;
+    public int puntos;
+    public String nombre = "";
+    public String info = "";
+    public boolean estado; 
+    
+    public Vehiculos (int id, int nivel, int calidad, int marca, boolean estado){
+        
+        this.id = id;
+        this.nivel = nivel;
+        this.contador = 0;
+        this.calidad = calidad;
+        this.marca = marca;
+        this.puntos = puntos;
+        this.estado = estado;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    } 
+    
+    public String getInfo() {
+        return info;
+    }
+    
+    public void setInfo(String info){
+        this.info = info;
+    }
+    
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
+    
+    public void setCalidad(int calidad){
+        this.calidad = calidad;
+    }
+    
+    public void sumContador(){
+        this.contador++;
+    }
+    
+    public void resetContador(){
+        this.contador = 1;
+    }
 }
