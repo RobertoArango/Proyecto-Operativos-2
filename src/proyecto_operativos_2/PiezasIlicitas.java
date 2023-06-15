a5b7cfa7e020cab65d94f6b4c7aa37cd7091c600
Victor Llanos <victor.llanos@correo.unimet.edu.ve>
2023-06-15 13:56:38 -0400

    creada clase (tentativa) logica para decidir ganador

---
+++ src/proyecto_operativos_2/PiezasIlicitas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_operativos_2;
+
+/**
+ *
+ * @author Vito
+ */
+
+//Las empreseas de vehiculos para tener mas posibilidades idearon utilizar piezas ilicitas
+//que mejoren el rendimiento a la hora de la carrera, cada pieza tendra un puntaje que
+//se le sumara a un total, con esto se decidira el ganador
+
+public class PiezasIlicitas {
+    
+}
