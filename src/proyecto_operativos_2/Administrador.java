1bf21647cc40aed156628c16b57b5848b3e7059b
RobertoArango <roberto.arango@correo.unimet.edu.ve>
2023-06-15 16:59:03 -0400

    variables inicializadas en main y arreglos en admin

--- src/proyecto_operativos_2/Administrador.java
+++ src/proyecto_operativos_2/Administrador.java
@@ -5,29 +5,27 @@
  */
 package proyecto_operativos_2;
 
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
 /**
  *
  * @author rober
  */
+
 public class Administrador {
     
     private Vehiculos retornarVehiculoAgregado(int calidad, int marca, boolean estado){
         //estos valores son placeholders, mas adelante cuando detallemos la sumatoria de calidades se dictara rl valor condicional
         if (calidad <= 1) {
-            int id = //hay que dirigirlo al id del main
+            int id = main.id++;
             Vehiculos vehiculo = new Vehiculos(id, 3, calidad, marca, estado);
             return vehiculo;
             
         } else if (calidad >= 2 && calidad <= 3){
-            int id = //hay que dirigirlo al id del main
+            int id = main.id++;
             Vehiculos vehiculo = new Vehiculos(id, 2, calidad, marca, estado);
             return vehiculo;
             
         } else { 
-            int id = //ha que dirigirlo al id del main
+            int id = main.id++;
             Vehiculos vehiculo = new Vehiculos(id, 1, calidad, marca, estado);
             return vehiculo;
         }
